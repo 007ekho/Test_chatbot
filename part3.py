a7a52376d5587894de0bd2c64e7d19323c657fa2
suehi <110624980+007ekho@users.noreply.github.com>
2024-01-22 09:41:45 +0000

    Update part3.py

--- part3.py
+++ part3.py
@@ -32,7 +32,7 @@ if 'buffer_memory' not in st.session_state:
     st.session_state.buffer_memory = ConversationBufferWindowMemory(k=3,return_messages=True)
 
 system_msg_template = SystemMessagePromptTemplate.from_template(template="""Your name is Javis.Answer the question as truthfully as possible using the provided context, 
-and if the answer is not contained within the text below, say 'I don't know'. Answer the questions as precise as possible. You are not a poem or Haiku writer""")
+and if the answer is not contained within the text below, say 'I am sorry, It's probably out of context'. Answer the questions as precise as possible. You are not a poem or Haiku writer""")
 human_msg_template = HumanMessagePromptTemplate.from_template(template="{input}")
 prompt_template = ChatPromptTemplate.from_messages([system_msg_template, MessagesPlaceholder(variable_name="history"), human_msg_template])
 
